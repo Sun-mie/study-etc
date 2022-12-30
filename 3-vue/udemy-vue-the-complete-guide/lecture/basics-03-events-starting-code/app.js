23ced619886bd0117885ea0c53cd257bfe597964
Sun-mie <ssunm2@naver.com>
2022-12-30 12:36:40 +0900

    move folder

---
+++ 3-vue/udemy-vue-the-complete-guide/lecture/basics-03-events-starting-code/app.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name:'',
+      confirmdName:'',
+      enteredName: ''
+    }
+  },
+  methods:{
+    add(num){
+      this.counter = this.counter + num;
+    },
+    reduce(num){
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName){
+      // 이벤트가 발생한 요소에 엑세스
+      // this.name = event.target.value;
+      // 두개 이상의 인자를 받아 처리하는 경우 vue에 내장된 $event를 사용할 수 있다.
+      this.name = event.target.value + ' ' + lastName;
+    },
+  submitForm(event){
+    alert('Submitted');
+  },
+  confirmInput(){
+    this.confirmdName = this.name
+  },
+  enterName(e){
+    this.name = e.target.value;
+  },
+  enteredName(){
+    this.enteredName = this.name;
+  }
+  }
+});
+
+app.mount('#events');
---
+++ README.md
@@ -0,0 +1,18 @@
+## study-development 
+
+💡개발에 관한 공부 내용을 push하고 있습니다.
+
+---
+
+### 폴더 설명
+
+1. `java` : 자바 공부
+2. `javaScript` : 자바스크립트 공부
+    1. modern-javascript-tutorial
+        
+        based on 모던 JavaScript 튜토리얼 ([https://ko.javascript.info/](https://ko.javascript.info/))
+        
+3. `vue` : vue 공부
+    1. udemy-vue-the-complete-guide
+4. `etc` : 그외 공부
+    1. `ezen-academy` : 국비과정 중 servlet 과제
